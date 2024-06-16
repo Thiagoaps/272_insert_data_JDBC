0d710f0ab3708750b6f63e1c2de234881b32f8e9
Thiago Antonio <thiagoapsx@gmail.com>
2024-06-16 18:14:39 -0300

    JDBC class, insert data second part

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -23,7 +25,8 @@ public class Program {
 					"INSERT INTO seller "
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES"
-					+ "(?, ?, ?, ?, ?)");		
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);		
 			
 			st.setString(1, "Carl Purple");
 			st.setString(2, "carl@gmail.com");
@@ -31,9 +34,24 @@ public class Program {
 			st.setDouble(4, 3000.00);
 			st.setInt(5, 4);
 			
+			
+			//EXAMPLE 2:
+			//st = conn.prepareStatement(
+			//		"insert into department (Name) values ('D1'),('D2')", 
+			//		Statement.RETURN_GENERATED_KEYS);
+			
 			int rowsAffected = st.executeUpdate();
 			
-			System.out.println("Done! Rows affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rown affected!");
+			}
 		
 		}catch (SQLException e) {
 			e.printStackTrace();
@@ -43,7 +61,7 @@ public class Program {
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();		
-		}		
-	}
+		}
+	}	
 }	
 
